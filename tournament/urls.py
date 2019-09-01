b8327b67107f679924717690a7ad02a65d0b9a71
ebergard <ebergard-xu@bk.ru>
2019-09-01 17:54:11 +0500

    comment

--- tournament/urls.py
+++ tournament/urls.py
@@ -4,18 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('success/', views.success, name='success'),
-    path('failure/', views.failure, name='failure'),
     path('participants/', views.participants, name='participants'),
-    path('rating/', views.rating, name='rating'),
     path('games/', views.games, name='games'),
+    path('rating/', views.rating, name='rating'),
+    path('playoff/', views.playoff, name='playoff'),
     path('accounts/register/', views.account_register, name='account_register'),
     path('accounts/login/', views.account_login, name='account_login'),
     path('accounts/logout/', views.account_logout, name='account_logout'),
     path('accounts/me/', views.me, name='me'),
     path('accounts/me/register/', views.me_register, name='me_register'),
+    path('accounts/me/before_draw/', views.me_before_draw, name='me_before_draw'),
     path('accounts/me/games/', views.me_games, name='me_games'),
     path('accounts/me/games/<game>/', views.me_games, name='me_game'),
-    path('accounts/me/before_draw/', views.me_before_draw, name='me_before_draw'),
 ]
--- tournament/views.py
+++ tournament/views.py
@@ -32,64 +32,28 @@ def index(request):
     return render(request, 'tournament/index.html', locals())
 
 
-def register(request):
-    tournament = get_current_tournament()
-    if tournament:
-        tournament_status = tournament.get_status()
-    else:
-        tournament_status = 5
-
-    if tournament_status == 0:
-        register_open = True
-        msg = "Регистрация на турнир:"
-    else:
-        register_open = False
-        msg = "Открытых регистраций нет"
-
-    if register_open:
-        if request.method == 'POST':
-            form = RegisterForm(request.POST)
-            if form.is_valid():
-                p = Participant(tournament=tournament,
-                                first_name=form.cleaned_data['first_name'],
-                                last_name=form.cleaned_data['last_name'],
-                                email=form.cleaned_data['email'])
-                try:
-                    p.save()
-                except IntegrityError:
-                    return HttpResponseRedirect('/failure')
-                return HttpResponseRedirect('/success')
-        else:
-            form = RegisterForm()
-
-    return render(request, 'tournament/register.html', locals())
-
-
-def success(request):
-    success = True
-    tournament = get_current_tournament()
-    tournament_status = tournament.get_status()
-    return render(request, 'tournament/register_result.html', locals())
-
-
-def failure(request):
-    success = False
-    tournament = get_current_tournament()
-    tournament_status = tournament.get_status()
-    return render(request, 'tournament/register_result.html', locals())
-
-
 def participants(request):
     tournament = get_current_tournament()
     tournament_status = tournament.get_status()
-    if tournament_status in (0, 1, 2):
+    if tournament_status in (0, 1, 2, 3):
         return render(request, 'tournament/participants.html', locals())
     else:
         return HttpResponse("<h2>Participants list is not available</h2>")
 
 
+def games(request, game=None):
+    tournament = get_current_tournament()
+    tournament_status = tournament.get_status()
+    games = tournament.game_set.all().order_by('game_date', 'start_time')
+    if tournament_status in (0, 1, 2, 3):
+        return render(request, 'tournament/games.html', locals())
+    else:
+        return HttpResponse("<h2>Games list is not available</h2>")
+
+
 def rating(request):
     tournament = get_current_tournament()
+    tournament_status = tournament.get_status()
     for p in tournament.participant_set.all():
         p.win_sets = 0
         p.win_balls = 0
@@ -112,39 +76,14 @@ def rating(request):
     participants = list(tournament.participant_set.all())
     participants.sort(key=lambda elem: (elem.win_sets, elem.win_balls), reverse=True)
 
-    tournament_status = tournament.get_status()
-    if tournament_status in (0, 1, 2):
+    if tournament_status in (0, 1, 2, 3):
         return render(request, 'tournament/rating.html', locals())
     else:
         return HttpResponse("<h2>Participants rating is not available</h2>")
 
 
-def games(request, game=None):
-    tournament = get_current_tournament()
-    tournament_status = tournament.get_status()
-    games = tournament.game_set.all().order_by('game_date', 'start_time')
-
-    if request.method == 'POST':
-        form = ResultForm(request.POST)
-        if form.is_valid():
-            game = Game.objects.get(pk=game)
-            for i in range(1, 6):
-                s = SetResult(game=game,
-                              set_number=i,
-                              result1=form.cleaned_data['set{}res1'.format(i)],
-                              result2=form.cleaned_data['set{}res2'.format(i)])
-                try:
-                    s.save()
-                except IntegrityError:
-                    return HttpResponseRedirect('/failure')
-            return HttpResponseRedirect('/games')
-    else:
-        form = ResultForm()
-
-    if tournament_status in (0, 1, 2):
-        return render(request, 'tournament/games.html', locals())
-    else:
-        return HttpResponse("<h2>Games list is not available</h2>")
+def playoff(request):
+    pass
 
 
 def account_register(request):
